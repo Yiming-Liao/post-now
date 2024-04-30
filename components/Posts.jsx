2a43494e752e903a2fc18c587abee14c8e53322f
Yiming Liao <yimingliao.official@gmail.com>
2024-04-30 11:13:09 +0800

    TEST

--- components/Posts.jsx
+++ components/Posts.jsx
@@ -6,21 +6,16 @@ import Card from './Card';
 const Posts = () => {
     const [allPosts, setAllPosts] = useState([]);
 
+
     useEffect(() => {
+
         const fetchPosts = async () => {
             const response = await fetch("/api/posts/all");
             const data = await response.json();
             setAllPosts(data);
         }
-
         // 初始載入時抓取一次資料
         fetchPosts();
-
-        // 設置定時器，每 2 秒抓取一次資料
-        const intervalId = setInterval(fetchPosts, 2000);
-
-        // 清理函數：組件卸載時停止定時器
-        return () => clearInterval(intervalId);
     }, []);
 
     return (
